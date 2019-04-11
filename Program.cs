3ba4e6a351db42de7e689e0f6b5410a191ad4cd6
West, Abby <abbyrosewest@gmail.com>
2019-04-11 17:53:48 -0400

    Added error catching

--- Program.cs
+++ Program.cs
@@ -10,19 +10,22 @@ namespace TextGenerator
     {
         static void Main(string[] args)
         {
-            //create list of lines
-            List<string> lines = new List<string>(LineLister());
+            for (int i = 0; i < 15; i++)
+            {
+                //create list of lines
+                List<string> lines = new List<string>(LineLister());
 
-            //remove punctuation and upper case chars become lower?
-            List<string> cleanLines = CleanText(lines);
+                //remove punctuation and upper case chars become lower?
+                List<string> cleanLines = CleanText(lines);
 
-            //convert each line into a list of Word class items
-            List<Word> WordList = CreateWords(cleanLines);
+                //convert each line into a list of Word class items
+                List<Word> WordList = CreateWords(cleanLines);
 
-            //parse together string
-            string phrase = StringMaker(WordList);
+                //parse together string
+                string phrase = StringMaker(WordList);
 
-            Console.WriteLine(phrase);
+                Console.WriteLine(phrase);
+            }
             Console.ReadKey();
         }
 
@@ -33,7 +36,7 @@ namespace TextGenerator
             string line;
             List<string> listOfLines = new List<string>();
 
-            StreamReader file = new StreamReader("../../../TextFile.txt");
+            StreamReader file = new StreamReader("../../../tweets.txt");
             while((line = file.ReadLine()) != null)
             {
                 listOfLines.Add(line);
@@ -139,16 +142,27 @@ namespace TextGenerator
             s1.Clear();
             currentWord = start;
 
-            while (!currentWord.isLast)
-            {
-                string next = currentWord.followedby[r.Next(0, currentWord.followedby.Count)].text;
-                Word nextWord = wordList.First(x => x.text == next);
-                s2.Append(nextWord.text + ' ');
-                currentWord = nextWord;
-            }
+            
+                while (!currentWord.isLast)
+                {
+                try
+                {
+                    string next = currentWord.followedby[r.Next(0, currentWord.followedby.Count)].text;
+                    Word nextWord = wordList.First(x => x.text == next);
+                    s2.Append(nextWord.text + ' ');
+                    currentWord = nextWord;
+                }
+                catch
+                {
+                    break;
+                }
+                }
 
-            return s2.ToString().Trim() + '.';
-        }
+                return s2.ToString().Trim() + '.';
+            
+            
+
+            }
     }
 
     public class Word
